59041c9a7cf61f02ed197732c9e83887ff467a70
Jhonatan <jacunitahuisa@gmail.com>
2021-10-19 23:54:16 -0500

    Auto stash before merge of "main" and "origin/main"

---
+++ app/src/main/java/com/lab04/myapplication/Model/Person.java
@@ -0,0 +1,17 @@
+package com.lab04.myapplication.Model;
+
+import java.util.*;
+
+class Person {
+    String nombres;
+    String apePat;
+    String apeMat;
+    Date fecNac;
+    char sexo;
+    String dni;
+    String direccion;
+
+    public Person(String nombres,String apePat, String apeMat,String dni, String direccion){
+
+    }
+}
