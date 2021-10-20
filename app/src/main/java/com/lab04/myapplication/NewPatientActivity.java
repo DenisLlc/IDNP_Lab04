3ed0fa954174c98b7cf2dbd62311f02d0ab95908
DenisLlc <dllamoca665@gmail.com>
2021-10-19 23:30:09 -0500

    layouts and others

---
+++ app/src/main/java/com/lab04/myapplication/NewPatientActivity.java
@@ -0,0 +1,14 @@
+package com.lab04.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewPatientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_patient);
+    }
+}
